f5e03ead2ffb1fed3d883438d6dc1f228e200111
dukai289 <dukai289@yeah.net>
2024-06-21 16:26:01 +0800

    20240621

--- train_gpt2.py
+++ train_gpt2.py
@@ -3,28 +3,72 @@
 from dataclass import dataclass
 from torch import nn
 import torch
-from torch.nn import functiional as F
+from torch.nn import functional as F
+import math
 
-@dataclass
-class TrainConfig:
-    block_size: int = 1024
-    vocab_size: int = 50257
-    n_layer: int = 12
-    n_head: int = 12
-    n_embed: int = 768
+
+
+FLASH = 0
+
+class NewGELU(nn.Module):
+    """Careful there are a few versions of GeLU, this one is the exact one used by OpenAI"""
+    def forward(self, input):
+        return 0.5 * input * (1.0 + torch.tanh(math.sqrt(2.0 / math.pi) * (input + 0.044715 * torch.pow(input, 3.0))))
 
 class CasualSelfAttention(nn.Module):
-    def __init__(self, config: TrainConfig):
+    def __init__(self, config):
         super().__init__()
-        ...
+        assert config.n_embed % config.n_head == 0
+        # key, query, value projections for all heads, but in batch
+        self.c_attn = nn.Linear(config.n_embed, 3* config.n_embed)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embed, config.n_embed)
+        self.c_proj.LLMC_RESIDUAL_SCALE_FLAG = 1
+        # regularization
+        self.n_head = config.n_head
+        self.n_embed = config.n_embed
+        # not really a 'bias', more of a mask, but following the OpenAI/HF naming though
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embed)
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim 
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embed, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)   
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs) 
+        if FLASH:
+            # flashattention
+            y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
+        else:
+            # manual implementation of attention
+            # this materializes the large(T, T) matrix for all the queries and keys
+            att = (q @ k.tranpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+            att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('inf'))
+            att = F.softmax(att, dim=-1)
+            y = att @ v # (B, nh, T ,T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        # output projection
+        y = self.c_proj(y)
+        return y
 
 class MLP(nn.Module):
-    def __init__(self, config: TrainConfig):
+    def __init__(self, config):
         super().__init__()
-        ...
+        self.c_fc = nn.Linear(config.n_embed, 4 * config.n_embed)
+        self.gelu = NewGELU()
+        self.c_proj = nn.Linear(4 * config.n_embed, config.n_embed)
+        self.c_proj.LLMC_RESIDUAL_SCALE_FLAG = 1
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
 
 class Block(nn.Module):
-    def __init__(self, config: TrainConfig):
+    def __init__(self, config):
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embed)
         self.attn = CasualSelfAttention(config)
@@ -34,6 +78,14 @@ class Block(nn.Module):
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
         return x
+
+@dataclass
+class TrainConfig:
+    block_size: int = 1024
+    vocab_size: int = 50257
+    n_layer: int = 12
+    n_head: int = 12
+    n_embed: int = 768
 
 class GPT(nn.Module):
     def __init__(self, config: TrainConfig):
